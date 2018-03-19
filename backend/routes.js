9dd6ac153d585b5336d286a7f253d9d30e5b6402
Shivang44 <shivangs44@gmail.com>
2018-03-19 16:31:16 -0400

    Basic registration working!

--- backend/routes.js
+++ backend/routes.js
@@ -1,3 +1,11 @@
+let sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
 function register_routes(app) {
     // When somebody goes to ourdomain.com/, reply with "Hello world"
     app.get('/', (req, res) => {
@@ -5,9 +13,63 @@ function register_routes(app) {
         res.send('Hello World!');
     });
 
-    app.post('/cat', (req, res) => {
-        // When somebody posts data to ourdomain.com/cat, do the following:
-        // Nada
+
+    /*
+    EXAMPLE SQLLITE + NODEJS CODE:
+
+    let sql = `SELECT DISTINCT Name name FROM playlists
+           ORDER BY name`;
+
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach((row) => {
+        console.log(row.name);
+      });
+    });
+
+    */
+
+
+    /*
+    * This endpoint is called when our app wants to register a user.
+    * Parameters needed in payload:
+    *       - email
+    *       - password (will be used to encrypt user's notes)
+    *
+    *
+    */
+    app.post('/register', (req, res) => {
+        // TODO: Validate these params, return error if not present
+        let email = req.body.email;
+        let password = req.body.password;
+
+        // TODO: Check if email already exists. If it does, return error.
+        let emailExists = false;
+
+        if (!emailExists) {
+            // Create user by inserting new record into users table
+            db.run(`INSERT INTO Users(email, password) VALUES(?, ?)`, [email, password], function(err) {
+              if (err) {
+                return console.log(err.message);
+              }
+              // get the last insert id
+              console.log(`A row has been inserted with rowid ${this.lastID}`);
+            });
+        }
+    });
+
+
+    /*
+    * This endpoint is called the user wants to login.
+    * Parameters needed in payload:
+    *       - email
+    *       - password
+    *
+    */
+    app.post('/login', (req, res) => {
+
     });
 }
 
