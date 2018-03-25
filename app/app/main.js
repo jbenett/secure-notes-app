3778d8ca6ec7f75f2ff133d52a0c33c47c919b4c
Unknown <lenzotti.5@buckeyemail.osu.edu>
2018-03-25 14:07:51 -0400

    communication between backend and front end

--- app/app/main.js
+++ app/app/main.js
@@ -58,3 +58,15 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+var request = require('request');
+
+request.post(
+    'http://localhost:3000/register',
+    { json: { email: 'hello@mail.com', password: 'pass1234' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
